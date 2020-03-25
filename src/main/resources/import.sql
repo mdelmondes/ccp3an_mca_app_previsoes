7f2116b0a021008215da06071c40f03c147e759b
Matheus Delmondes <mdelmondes@icloud.com>
2020-03-24 23:13:54 -0300

    Inclusao do restante dos codigos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (1, 'Terça-Feira', 22.0, 31.9, 57, 'Sol. Pancadas de chuva a qualquer hora do dia e da noite. Muitas nuvens de manhã.');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (2, 'Quarta-Feira', 21.7, 28.7, 65, 'Sol e aumento de nuvens de manhã. Pancadas de chuva à tarde e à noite.');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (3, 'Quinta-Feira', 24.1, 29.0, 64, 'Sol e aumento de nuvens de manhã. Pancadas de chuva à tarde e à noite.');
